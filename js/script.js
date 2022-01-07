353a94d8fbb0d6b896a29282c92c2b14f079d8c3
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2022-01-07 16:17:08 +0000

    Added ballReset function at script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 // Set Up Global Variables and Constants
-const canvas = document.getElementsByTagName('canvas')[0];
+let canvas = document.getElementsByTagName('canvas')[0];
 let canvasContext;
 let ballX = 50;
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 let ballY = 50;
-let ballSpeexX = 18;
+let ballSpeedX = 18;
 let ballSpeedY = 16;
 let player1Score = 0;
 let player2Score = 0;
@@ -28,4 +28,41 @@ function calculateMousePos(e) {
         x: mouseX,
         y: mouseY
     };
+}
+
+// Function to Handle Mouse Click
+function handleMouseClick(e) {
+    if (showingWinScreen) {
+        player1Score = 0;
+        player2Score = 0;
+        showingWinScreen = false;
+    }
+}
+
+// Load the Object
+window.onload = () => {
+    canvas = document.getElementById('gamecanvas');
+    canvasContext = canvas.getContext('2d');
+    let framesPerSecond = 30;
+    setInterval(function () {
+        moveEverything();
+        drawEverything();
+    }, 1000 / framesPerSecond);
+
+    canvas.addEventListener('mousedown',handleMouseClick);
+    canvas.addEventListener('mousemove',function(e) {
+        let mousePos = calculateMousePos(e);
+        paddle1Y = mousePos.y - (PADDLE_HEIGHT/2);
+    });
+};
+
+// Function to Reset Ball
+function ballReset() {
+    if (player1Score >= WINNING_SCORE || player2Score >= WINNING_SCORE) {
+        showingWinScreen = true;
+    }
+
+    ballSpeedX = -ballSpeedX;
+    ballX = canvas.width/2;
+    ballY = canvas.height/2;
 }
